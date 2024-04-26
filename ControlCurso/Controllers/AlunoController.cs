6f43ccb4bd365e887cf5d290a1f79b29c7e437c0
pedro.s.nogueira <pedro.s.nogueira@WINAPWPIQQITJCI>
2024-04-26 16:05:00 -0300

    Add project files.

---
+++ ControlCurso/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControlCurso.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
